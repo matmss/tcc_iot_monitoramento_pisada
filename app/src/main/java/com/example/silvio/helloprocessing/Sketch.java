5525ef3b50dae828f8c7c57ca0f398c381d26630
matmss <matheus_jp08@hotmail.com>
2019-09-22 23:24:46 -0300

    Initial commit

---
+++ app/src/main/java/com/example/silvio/helloprocessing/Sketch.java
@@ -0,0 +1,17 @@
+package com.example.silvio.helloprocessing;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    public void settings() {
+        size(600, 600);
+    }
+
+    public void setup() { }
+
+    public void draw() {
+        if (mousePressed) {
+            ellipse(mouseX, mouseY, 50, 50);
+        }
+    }
+}
